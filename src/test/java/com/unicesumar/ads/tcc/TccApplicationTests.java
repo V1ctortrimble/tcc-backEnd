30817fb7fd4a11ef62e055a99af0704e1b576bfa
Victor <victorif627@outloo.com>
2020-08-26 21:30:29 -0300

    [Victor][TSK017]Start de projeto Spring mais configurações de Swagger e H2Database

---
+++ src/test/java/com/unicesumar/ads/tcc/TccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicesumar.ads.tcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
