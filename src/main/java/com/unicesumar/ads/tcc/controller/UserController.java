88d829d2dc3921e52e330a93aed85146e2429ae9
Gustavo Santos <guto_rick1997@hotmail.com>
2020-09-17 21:44:13 -0300

    [Gustavo][TSK032]Implementa autenticação

--- src/main/java/com/unicesumar/ads/tcc/controller/UserController.java
+++ src/main/java/com/unicesumar/ads/tcc/controller/UserController.java
@@ -1,4 +1,16 @@
 package com.unicesumar.ads.tcc.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @RequestMapping("/users")
+    @ResponseBody
+    public String getUsers() {
+        return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+                "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+    }
 }
