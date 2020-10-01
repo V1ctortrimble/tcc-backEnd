91af5d5e08e5aba22e93c2972bbbcfe42ccf04f0
Marcio L Lesniovski <marcio.lesni@gmail.com>
2020-10-01 12:04:21 -0300

    [Marcio][TSK032]Corrige link recuperação de senha

--- src/main/java/com/unicesumar/ads/tcc/service/RecoveryPassConstants.java
+++ src/main/java/com/unicesumar/ads/tcc/service/RecoveryPassConstants.java
@@ -5,7 +5,7 @@ package com.unicesumar.ads.tcc.service;
  */
 public class RecoveryPassConstants {
 
-    public static final String LINK = "https://localhost:8000/recuperasenha/";
+    public static final String LINK = "http://localhost:3000/recoverPassword/";
     public static final String RESPONSE = "Usuário não encontrado!!!";
     public static final String TEXT = "Você solicitou a recuperação de senha, clique no link para " +
             "realizar o cadastro de uma nova senha: ";
