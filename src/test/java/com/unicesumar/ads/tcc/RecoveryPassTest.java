e6192e8e2cdbf05a0ebafbcf2da1acdeda529fd3
Marcio L Lesniovski <marcio.lesni@gmail.com>
2020-10-04 12:45:48 -0300

    [Marcio][TSK038]Implementa caso de teste

--- src/test/java/com/unicesumar/ads/tcc/RecoveryPassTest.java
+++ src/test/java/com/unicesumar/ads/tcc/RecoveryPassTest.java
@@ -2,12 +2,18 @@ package com.unicesumar.ads.tcc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.unicesumar.ads.tcc.controller.RecoveryPassController;
+import com.unicesumar.ads.tcc.data.repository.UsersRepository;
+import com.unicesumar.ads.tcc.dto.UsersDTO;
 import com.unicesumar.ads.tcc.service.RecoveryPassService;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -30,14 +36,28 @@ public class RecoveryPassTest {
 
     @Test
     void case1() throws Exception{
-        String email = "marcio.lesni@gmail.com";
 
-        RecoveryPassService recoveryPassService = new RecoveryPassService();
+        UsersDTO usersDTO = new UsersDTO();
+        usersDTO.setUsername("marcio.lesni@gmail.com");
+
+        mockMvc.perform(post("/sendemail")
+                .contentType("application/json")
+        .content(objectMapper.writeValueAsString(recoveryPassController.sendRecoveryEmail(usersDTO))))
+        .andExpect(status().isOk());
+
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void case2() throws Exception{
+
+        UsersDTO usersDTO = new UsersDTO();
+        usersDTO.setUsername("");
 
         mockMvc.perform(post(BASE_URL+"/sendemail")
                 .contentType("application/json")
-        .content(objectMapper.writeValueAsString(recoveryPassService.recoveryPass(email))))
-        .andExpect(status().isOk());
+                .content(objectMapper.writeValueAsString(recoveryPassController.sendRecoveryEmail(usersDTO))))
+                .andExpect(status().isBadRequest());
 
         Assertions.assertTrue(true);
 
