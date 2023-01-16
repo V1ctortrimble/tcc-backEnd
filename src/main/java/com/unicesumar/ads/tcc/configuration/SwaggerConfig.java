83ab2872b17602e09f42886d550937dea98578b9
V1ctortrimble <victorif627@outlook.com>
2023-01-16 18:21:58 -0300

    Prepara ambiente para correção

--- src/main/java/com/unicesumar/ads/tcc/configuration/SwaggerConfig.java
+++ src/main/java/com/unicesumar/ads/tcc/configuration/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.unicesumar.ads.tcc.configuration;
 
-import io.swagger.models.Swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -9,11 +8,9 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.ApiSelector;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 /**
